7f5ca2aba8cd7b7f5dfa9173f901a48e8725e959
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-06-01 12:58:57 +0100

    add workout comp

--- ui/WorkoutCard.tsx
+++ ui/WorkoutCard.tsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { observer } from "mobx-react-lite";
 interface Props {
-
-}
-
-export const WorkoutCard: React.FC<Props> = () => {
-	return (
-        <View style={styles.card}>
-          <Text>hello Olaide</Text>
-        </View>
-		);
+ excercise: string;
+ repsAndWeight: string;
+ sets: string[];
+ onSetPress: (index: number) => void;
 }
 
 const styles = StyleSheet.create({
@@ -21,5 +16,78 @@ const styles = StyleSheet.create({
   	shadowOffset: { width: 2, height: 2},
   	shadowOpacity: 0.3,
   	shadowRadius: 3,
+  	flexDirection: 'column',
+  	padding: 10,
+    marginBottom: 10
+  },
+  topRow: {
+  	flexDirection: 'row',
+  	justifyContent: 'space-between'
+  },
+  topRowText: {
+  	fontSize: 16
+  },
+  bottomRow: {
+  	flexDirection: 'row',
+  	justifyContent: 'space-between',
+  	marginTop: 14,
+  },
+  circle: {
+  	height: 50,
+  	width: 50,
+  	borderRadius: 25,
+  	backgroundColor: '#8FB299',
+  	
+
+  },
+  whiteText: {
+    color: '#fff',
+  },
+  circleText: {
+  	fontSize: 16,
+  	margin: 'auto',
+  },
+  fadedBackground: {
+  	backgroundColor: '#B2A1A1',
+  },
+  grayText: {
+  	color: '#655252'
   }
 })
+
+export const WorkoutCard: React.FC<Props> = observer(({excercise, repsAndWeight, sets, onSetPress}) => {
+	return (
+        <View style={styles.card}>
+          
+           <View style={styles.topRow}>
+	           <Text style={styles.topRowText}>{excercise}</Text> 
+	           <Text style={styles.topRowText}>{repsAndWeight}</Text>     
+           </View>
+
+           <View style={styles.bottomRow}>
+               {sets.map((set, index) => {
+               	if (set === 'x') {
+               		return (
+  						<View style={[styles.circle, styles.fadedBackground]} key={set + index}>
+               			   <Text style={[styles.circleText,styles.grayText]}>x</Text>
+               		    </View>
+               		)
+               	}
+               	if (set === '') {
+               	return (
+               			<TouchableOpacity onPress={() => onSetPress(index)}  style={[styles.circle, styles.fadedBackground]} key={set + index} />
+               	   )
+               	}
+
+               	return (
+               		<TouchableOpacity onPress={() => onSetPress(index)} style={styles.circle} key={set + index}>
+               			<Text style={[styles.whiteText, styles.circleText]}>{set}</Text>
+               		</TouchableOpacity>
+               	)
+               })}
+           </View>
+
+        </View>
+		);
+})
+
