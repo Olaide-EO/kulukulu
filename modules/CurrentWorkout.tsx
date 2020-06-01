74d4cc71b67506707ee0f6af1be4bd4408f1918d
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-06-01 12:55:38 +0100

    add touchables

--- modules/CurrentWorkout.tsx
+++ modules/CurrentWorkout.tsx
@@ -1,25 +1,51 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { observer } from "mobx-react-lite";
-import { RouterStoreContext } from "../store/RouterStore";
+import { RootStoreContext } from "../store/RootStore";
 import { WorkoutCard } from "../ui/WorkoutCard"
-
+import { WorkoutTimer } from "../ui/WorkoutTimer";
 interface Props {
 
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "fafafa",
+    
     margin: 10,
+    height: '100%'
   }
 })
 
  const CurrentWorkout: React.FC<Props> = observer(() => {
-     const routerStore = React.useContext(RouterStoreContext);
+     const rootStore = React.useContext(RootStoreContext);
+
 	return (
      <View style={styles.container}>
-        <WorkoutCard />
+       {rootStore.workoutStore.currentExercises.map(e => {
+         
+      return(
+        <WorkoutCard 
+         onSetPress={setIndex => {
+           const v = e.sets[setIndex];
+           let newValue: string;
+           if( v === ''){
+             newValue = `${e.reps}`
+           }else if (v === '0') {
+             newValue = ''
+           } else {
+             newValue = `${parseInt(v) - 1}`
+           }
+           e.sets[setIndex] = newValue;
+         }}
+         key={e.exercise}
+         sets={e.sets} 
+         excercise={e.exercise} 
+         repsAndWeight={`${e.numSets}x${e.reps} ${e.weight}`}
+         />
+        )    
+
+      })}
+       <WorkoutTimer onXPress={() => {}} />
      </View>
 		);
 });
--- modules/WorkoutHistory.tsx
+++ modules/WorkoutHistory.tsx
@@ -1,20 +1,47 @@
 import * as React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { observer } from "mobx-react-lite";
-import { RouterStoreContext } from "../store/RouterStore";
+import { RootStoreContext } from "../store/RootStore";
 
 interface Props {}
 
 const WorkoutHistory: React.FC<Props> = observer(() => {
-	const routerStore = React.useContext(RouterStoreContext);
+	const rootStore = React.useContext(RootStoreContext);
+
 
 	return (
        <View>
-         <Text style={styles.text}>Workout History page whatever you want is here, Jesus loves you and will be your help all the days of your life</Text>
+         <Text style={styles.text}>Workout History Page</Text>
          <Button 
              title="create workout"  
              onPress={() => {
-             	routerStore.screen = "CurrentWorkout";
+               rootStore.workoutStore.currentExercises.push(
+               {
+                exercise: "Squat",
+                numSets: 5,
+                reps: 5,
+                sets: ["5", "5","5","5","5"],
+                weight: 260
+               },
+
+               {
+                exercise: "Bench Press",
+                numSets: 5,
+                reps: 5,
+                sets: ["5", "5","5","5","5"],
+                weight: 200
+               },
+
+               {
+                exercise: "Deadlift",
+                numSets: 1,
+                reps: 5,
+                sets: ["5", "x","x","x","x"],
+                weight: 360
+               }
+
+               );
+             	rootStore.routerStore.screen = "CurrentWorkout";
              }} 
           />
        </View>
@@ -22,14 +49,9 @@ const WorkoutHistory: React.FC<Props> = observer(() => {
 })
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
   text: {
-    color: '#fff'
+    color: '#000000'
   }
 
 });
