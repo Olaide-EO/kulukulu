0b9eaa75520b1cbd9a90c363610db685491c635f
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-06-01 12:59:24 +0100

    add timer comp

---
+++ ui/WorkoutTimer.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+
+interface Props {
+ 
+ onXPress: () => void; 	
+
+}
+
+const styles = StyleSheet.create({
+	container: {
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		height: 50,
+		width: '100%',
+		backgroundColor: '#486550',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		alignItems: 'center',
+		padding: 10,  
+	},
+	x: {
+		color: '#B2A1A1',
+		fontSize: 30
+	},
+	timeText: {
+		color: '#fff',
+		fontSize: 18
+	}
+})
+
+export const WorkoutTimer: React.FC<Props> = (onXPress) => {
+	return (
+         <View style={styles.container}>
+            <Text style={styles.timeText} > Hello Olaide</Text>
+
+            <TouchableOpacity onPress={onXPress}>
+            <Text style={styles.x}>x</Text>
+            </TouchableOpacity>
+         </View>
+		)
+}
