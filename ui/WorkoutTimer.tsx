267e8799d82f4e448462722a4e918e0da32d4255
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-06-03 12:49:46 +0100

    add percent line

--- ui/WorkoutTimer.tsx
+++ ui/WorkoutTimer.tsx
@@ -3,7 +3,9 @@ import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 interface Props {
  
- onXPress: () => void; 	
+ onXPress: () => void; 
+ currentTime: string;
+ percent: string;	
 
 }
 
@@ -15,10 +17,15 @@ const styles = StyleSheet.create({
 		height: 50,
 		width: '100%',
 		backgroundColor: '#486550',
-		justifyContent: 'space-between',
-		flexDirection: 'row',
-		alignItems: 'center',
-		paddingHorizontal: 30,  
+		
+		
+	},
+	row: {
+	  paddingHorizontal: 30,
+      justifyContent: 'space-between',
+	  flexDirection: 'row',
+	  alignItems: 'center',
+	  flex: 1,
 	},
 	x: {
 		color: '#B2A1A1',
@@ -27,17 +34,24 @@ const styles = StyleSheet.create({
 	timeText: {
 		color: '#fff',
 		fontSize: 18
+	},
+	line: {
+		height: 3,
+		backgroundColor: '#B2A1A1',
 	}
 })
 
-export const WorkoutTimer: React.FC<Props> = (onXPress) => {
+export const WorkoutTimer: React.FC<Props> = ({onXPress, currentTime, percent }) => {
 	return (
          <View style={styles.container}>
-            <Text style={styles.timeText} > Hello Olaide</Text>
+           <View style={[styles.line, { width: percent }]} />
+           <View style={styles.row}>
+            <Text style={styles.timeText} >{currentTime}</Text>
 
             <TouchableOpacity onPress={onXPress}>
             <Text style={styles.x}>x</Text>
             </TouchableOpacity>
+            </View>
          </View>
 		)
 }
