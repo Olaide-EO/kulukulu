15fc6f17e52f08487bbf2682f611756fe3b95afa
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-06-03 12:48:06 +0100

    add router params

---
+++ Routes.tsx
@@ -0,0 +1,14 @@
+import { RootStoreContext } from "./store/RootStore"
+import React, { useContext } from 'react';
+import { observer } from 'mobx-react-lite';
+import CurrentWorkout from './modules/CurrentWorkout';
+import WorkoutHistory from './modules/WorkoutHistory';
+
+
+const Routes = observer(() => {
+	const rootStore = useContext(RootStoreContext);
+
+	return rootStore.routerStore.screen === 'WorkoutHistory' ? <WorkoutHistory />: <CurrentWorkout />;
+})
+
+export default Routes;
