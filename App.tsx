56a2198a2578b0be48f0b1e71e72cf7bbb07f609
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2020-06-03 12:46:16 +0100

    change router path

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React, { useState, useContext } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {observer} from 'mobx-react-lite';
 import { CounterStoreContext } from "./store/CounterStore";
-import Router from './router/Router';
+import Routes from './Routes';
 
 const App = () => {
   const counterStore = useContext(CounterStoreContext);
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
-       <Router />
+       <Routes />
        </View>
       
     </View>
